a42e791ec229d54813745a9155f0d476ffbcbbf5
monica-ej-park <monica.ej.park@gmail.com>
2022-04-27 11:04:20 +0900

    modified

--- settings/base.py
+++ settings/base.py
@@ -41,8 +41,6 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '^f3n_&0pwefe%x3pf2)5@u-pz!8n@=r7zut-twj&3!1kx_vbkx'
 
 
 # Application definition
